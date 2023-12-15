58b5c1dd02f6b1a27ac2cd4da38e80ae1d3e7cbf
usilgtfoms <usilgtfoms@irkoms.ru>
2023-12-15 14:01:32 +0800

    Added Pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+let Pinia = createPinia();
+
+createApp(App).use(Pinia).mount('#app')
